87566042ff098ccb9344ceebdff75f2696d7e571
KOJI-YAMAMOTO-GitHub <43413251+KOJI-YAMAMOTO-GitHub@users.noreply.github.com>
2020-11-18 11:49:14 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+setInterval(() => {
+  // 現在時間の取得
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  // 時、分、秒を元に角度を計算
+  const dH = h * (360 / 12) + m * (360 / 12 / 60);
+  const dM = m * (360 / 60);
+  const dS = s * (360 / 60);
+
+  // 各要素を取得
+  const eH = document.querySelector(".clock_hour");
+  const eM = document.querySelector(".clock_min");
+  const eS = document.querySelector(".clock_sec");
+
+  // styleを追加
+  eH.style.transform = `rotate(${dH}deg)`;
+  eM.style.transform = `rotate(${dM}deg)`;
+  eS.style.transform = `rotate(${dS}deg)`;
+}, 10);
+
+setInterval(() => {
+  // 現在時間の取得
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  var elem = document.getElementById("time");
+  elem.textContent = now;
+}, 300);
